ee41cd48ad57d61f724e57a5853082137288babd
Umesh Rohidas Jadhav <umeshjadhavxig@gmail.com>
2024-09-30 15:49:09 +0530

    First Hero Page Change

---
+++ src/Components/HeroComponents/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Hero.css'
+const Hero = () => {
+  return (
+    <>
+     <nav className='container'>
+        <h2>Home</h2>
+        <h2>About</h2>
+        <h2>Contact</h2>
+     </nav>
+    </>
+  )
+}
+
+export default Hero
